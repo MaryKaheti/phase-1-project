988020da5868678d96482e52a69e791cc3ccb631
MaryKaheti <kahetikihara@gmail.com>
2023-04-04 11:10:53 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function displaydata(data) {
+function displayData(data) {
     let card = document.createElement("li");
     card.className = "card";
     card.innerHTML = `
-    <div class="card" style="width: 18rem;">
-  <img src=${data.image}class="card-img-top" alt=${data.title}>
-  <div class="card-body">
-    <p class="card-text">${data.description}</p>
-  </div>
-</div>`
+    <div>
+    <p><b>Title:</b> ${data.title}</p>
+    <p><b>Description:</b> ${data.description}</p>
+    <img src = ${data.image}>
+    </div>
+    
+    `;
+    document.querySelector("#data-list").appendChild(card);
 }
+function fetchData() {
+    fetch("http://localhost:3000/data")
+    .then((res) => res.json())
+    .then((data) => {
+        
+        const dataList = document.querySelector("#data-list");
+        
+        data.forEach((item) => {
+            const dataItem = document.createElement("li");
+            dataItem.className= "data item";
+            dataItem.textContent= item.title;
+            dataList.appendChild(dataItem);
+            dataItem.addEventListener("click", () => {
+                displayData(item)
+            })
+        })
+    })
+    .catch((error)=> console.log(error));
+}
+fetchData();
