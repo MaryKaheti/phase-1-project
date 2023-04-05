6f714b3e5f25fbca84347f4787e52eb5f65b52a0
MaryKaheti <kahetikihara@gmail.com>
2023-04-05 18:01:37 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -31,30 +31,58 @@ function fetchData() {
     .catch((error)=> console.log(error));
 }
 fetchData();
-
-function pageLoads() {
-    window.addEventListener("mouseover", () => {
-        
-    })
-}
-console.log(alert ("Thank you for being here..."))
+// pageLoads()
+// function pageLoads() {
+//     window.addEventListener("mouseover", () => {
+//         alert("Thank you for being here!!")
+//     })
+// }
 function displayProducts(products){
     let card = ''
     products.forEach(products => {
         card+=`
-        <div class = 'card' id="my-card">
+        <div class = 'card' id="my-card${products.id}">
         <img src = "${products.image}">
         <p> ${products.description}</p>
         <p> ${products.price}</p>
-        <button id="my-btn" padding-bottom = "20px">Buy</button>
+        <button type='button' id="my-btn" padding-bottom = "20px">Buy</button>
+        <button type='button' id="mydelete${products.id}" padding-bottom = "20px">Delete</button>
         </div>
         `
-    });
-    document.querySelector("#products-list").innerHTML = card
+
+        document.querySelector("#products-list").innerHTML = card
+
+deleteProduct(products)
+});
 }
 function fetchProducts() {
     fetch(" http://localhost:3000/products")
     .then(res => res.json())
     .then(products => displayProducts(products))
+
+
+
 }
 fetchProducts()
+function deleteProduct(products){
+
+    let deletebtn = document.getElementById(`mydelete${products.id}`)
+    deletebtn.addEventListener('click', (event) => {
+        console.log('reaches')
+fetch(`http://localhost:3000/products/${products.id}`, 
+{
+    method: 'DELETE',
+}).then((response)=>{
+if(response.OK){
+console.log('success')
+document.getElementById(`my-card${products.id}`).innerHTML = "";
+}else{
+console.log('failed')
+}
+});
+})
+}
+
+function updateProducts(products) {
+    let updatebtn = document.getElementById()
+}
